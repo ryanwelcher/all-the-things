c723e1badda52ac6972f9189040827ce99b331a6
Ryan Welcher <me@ryanwelcher.com>
2024-12-10 13:51:32 -0500

    Allow scripts

--- index.js
+++ index.js
@@ -2,27 +2,10 @@ const { join } = require( 'path' );
 
 module.exports = {
 	defaultValues: {
-		namespace: 'block-developers-cookbook',
-		category: 'block-developers-cookbook',
 		customPackageJSON: {
 			prettier: '@wordpress/prettier-config',
 		},
 		example: {},
-		wpScripts: false,
-		customPackageJSON: {
-			scripts: {
-				build: 'wp-scripts build',
-				format: 'wp-scripts format',
-				'lint:css': 'wp-scripts lint-style',
-				'lint:js': 'wp-scripts lint-js',
-				'packages-update': 'wp-scripts packages-update',
-				'plugin-zip': 'wp-scripts plugin-zip',
-				start: 'wp-scripts start',
-			},
-			devDependencies: {
-				'@wordpress/scripts': '^26.12.0',
-			},
-		},
 	},
 	variants: {
 		dynamic: {
